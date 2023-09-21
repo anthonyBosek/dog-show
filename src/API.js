0367af9d3a42966318f95cbc7294a44d920a6e0a
Anthony Bosek <anthony.bosek@gmail.com>
2023-09-21 08:56:31 -0700

    complete lab

---
+++ src/API.js
@@ -0,0 +1,43 @@
+const getData = (url) => {
+  return fetch(url).then((resp) => {
+    if (resp.ok) {
+      return resp.json();
+    } else {
+      throw resp.statusText;
+    }
+  });
+};
+
+const postData = (url, payload) => {
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(payload),
+  }).then((resp) => {
+    if (resp.ok) {
+      return resp.json();
+    } else {
+      throw resp.statusText;
+    }
+  });
+};
+
+const patchData = (url, payload) => {
+  return fetch(url, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(payload),
+  }).then((resp) => {
+    if (resp.ok) {
+      return resp.json();
+    } else {
+      throw resp.statusText;
+    }
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,77 @@
-document.addEventListener("DOMContentLoaded", () => {});
+//* Global Variables
+const DOGS_URL = "http://localhost:3000/dogs";
+let editId;
+const tableBody = document.getElementById("table-body");
+const dogForm = document.getElementById("dog-form");
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const name = e.target.name.value;
+  const breed = e.target.breed.value;
+  const sex = e.target.sex.value;
+  const payload = { name, breed, sex };
+  patchData(`${DOGS_URL}/${editId}`, payload)
+    .then((dog) => {
+      document.querySelector(`#tr-${dog.id} > td:nth-child(1)`).textContent =
+        dog.name;
+      document.querySelector(`#tr-${dog.id} > td:nth-child(2)`).textContent =
+        dog.breed;
+      document.querySelector(`#tr-${dog.id} > td:nth-child(3)`).textContent =
+        dog.sex;
+      e.target.reset();
+      editId = null;
+    })
+    .catch((err) => console.log("Error: ", err));
+};
+
+const appendEditInfo = (dogObj) => {
+  dogForm.name.value = dogObj.name;
+  dogForm.breed.value = dogObj.breed;
+  dogForm.sex.value = dogObj.sex;
+  editId = dogObj.id;
+};
+
+const appendAllDogs = (dogsArr) => {
+  dogsArr.forEach((dog) => {
+    const tr = document.createElement("tr");
+    tr.id = `tr-${dog.id}`;
+
+    const name = document.createElement("td");
+    name.textContent = dog.name;
+
+    const breed = document.createElement("td");
+    breed.textContent = dog.breed;
+
+    const sex = document.createElement("td");
+    sex.textContent = dog.sex;
+
+    const edit = document.createElement("td");
+    const btn = document.createElement("button");
+    btn.addEventListener("click", () => getOneDog(dog.id));
+    btn.textContent = "Edit Dog";
+    edit.appendChild(btn);
+
+    tr.append(name, breed, sex, edit);
+
+    tableBody.appendChild(tr);
+  });
+};
+
+const getOneDog = (_id) => {
+  getData(`${DOGS_URL}/${_id}`)
+    .then((dog) => appendEditInfo(dog))
+    .catch((err) => console.log("Error: ", err));
+};
+
+const getAllDogs = () => {
+  getData(DOGS_URL)
+    .then((allDogs) => appendAllDogs(allDogs))
+    .catch((err) => console.log("Error: ", err));
+};
+
+const init = () => {
+  getAllDogs();
+  dogForm.addEventListener("submit", handleSubmit);
+};
+
+init();
